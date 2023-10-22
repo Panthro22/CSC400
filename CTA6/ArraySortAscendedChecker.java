e1d57c11f400ec5b5ec135aa034c0e7bc0089f9e
Panthro22 <jordonpaynter22@gmail.com>
2023-10-22 13:16:44 -0600

    updated comments
    Thought process comments

--- CTA6/ArraySortAscendedChecker.java
+++ CTA6/ArraySortAscendedChecker.java
@@ -3,12 +3,13 @@ package CTA6;
 // Pg 458 Excersie # 11
 // Devise an algorithm that determines whether an array is sorted in ascending order. Write a
 // Java method that implements your algorithm. you can use your method to test whether a sort method has
-// executed correctly. it may be intger or an object or any thing else so use a generic method to check.
+// executed correctly.
 public class ArraySortAscendedChecker {
 
   // Big O: O(n) Since we have to iterate through the entire array to check if it is sorted.
   // Thought about comparing left side to right side working towards the middle for a big O of O(n/2) but
   // it would still be unable to garuntee that the array is 100% sorted.
+  // Recursive method would simply increase the space complexity and not improve the time complexity enough to be worth it.
   public static boolean isSortedAscending(int[] arr) {
     if (arr.length == 0 || arr.length == 1) {
       // An empty array or an array with a single element is sorted.
